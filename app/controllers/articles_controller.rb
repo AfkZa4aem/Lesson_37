5f964c2cccb37f2158f1fa404e8336c03b16ca20
afkza4aem <nastypinagrably@gmail.com>
2017-02-24 13:36:56 +0300

    Add index and update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,24 @@
 class ArticlesController < ApplicationController
+  def index
+    @article = Article.all
+  end
+
   def new
   end
 
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(article_params)
+    if @article.valid?
+      @article.save
+      render action: 'index'
+    else
+      render action: 'new'
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 end
