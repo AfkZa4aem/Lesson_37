36b74d3eb1b15ca1f1747c9a44ac17522edbe962
AfkZa4aem <nastypinagrably@gmail.com>
2017-03-14 20:48:48 +0300

    Add comment validation tests

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe Comment do
-  it { should belong_to :article }
+  describe "validations" do
+    it { should ensure_length_of(:body).is_at_most(400)}
+  end
+
+  describe "associations" do
+    it { should belong_to :article }
+  end
 end
