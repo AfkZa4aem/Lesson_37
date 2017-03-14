c06399519d98e286681a77fc44addd8e5ecf68b1
AfkZa4aem <nastypinagrably@gmail.com>
2017-03-14 20:04:33 +0300

    Update article spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -20,4 +20,15 @@ describe Article do
 
     end
   end
+
+  describe "#last_comment" do
+    it "returns the last comment" do
+      # create article with comments
+      article = create(:article_with_comments)
+
+      # assert
+      expect(article.last_comment.body).to eq "comment body 3"
+    end
+  end
+
 end
