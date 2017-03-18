1bcb938dfea5a7abab7583abdd422f6df653dfa1
afkza4aem <nastypinagrably@gmail.com>
2017-03-18 17:59:26 +0300

    Refactoring: move sign_up to session_helper.rb

---
+++ spec/support/session_helper.rb
@@ -0,0 +1,8 @@
+def sign_up
+  fill_in :user_email, with: 'user@example.com'
+  fill_in :user_username, with: 'username'
+  fill_in :user_password, with: 'secure123!@#'
+  fill_in :user_password_confirmation, with: 'secure123!@#'
+
+  click_button 'Sign up'
+end
